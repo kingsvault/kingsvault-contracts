55e610c121ae6389e959ec921c53a6f9c9850a7d
an-ivannikov <info@ivannikov.pro>
2025-03-26 21:04:44 +0400

    update

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+
+
+module.exports = {
+  solidity: "0.8.22",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
